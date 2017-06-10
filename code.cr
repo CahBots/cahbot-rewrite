1b4b8db88e104e0e7c49e587b8835e1f33ab3ed3
2003cah <2003cah@gmail.com>
2017-06-10 18:07:56 -0400

    Cah is an idiot 4.1.1

--- code.cr
+++ code.cr
@@ -40,8 +40,9 @@ client.on_message_create do |payload|
   if payload.content.starts_with? PREFIX + "urban"
     stuff = Urban.define("#{payload.content[8..-1]}")
     begin
-      client.create_message(payload.channel_id, "First result for #{payload.content[8..-1]}
+      client.create_message(payload.channel_id, "First result for `#{payload.content[8..-1]}`
 
+Word/Term: #{stuff.list.first.word}
 Author: #{stuff.list.first.author}
 Definition: #{stuff.list.first.definition}
 Example: *#{stuff.list.first.example}*
