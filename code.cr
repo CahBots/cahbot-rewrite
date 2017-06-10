99da5763ba36aeb848457750565eb37ab1ac898e
2003cah <2003cah@gmail.com>
2017-06-10 17:16:37 -0400

    Extra commands and no

--- code.cr
+++ code.cr
@@ -9,7 +9,7 @@ client.on_message_create do |payload|
   if payload.content.starts_with? PREFIX + "ping"
     m = client.create_message(payload.channel_id, "Pong!")
     time = Time.utc_now - payload.timestamp
-    client.edit_message(m.channel_id, m.id, "Pong! That took #{time.total_milliseconds.round(0)} ms.")
+    client.edit_message(m.channel_id, m.id, "Pong! That took around #{time.total_milliseconds.round(0)} ms.")
   end
 end
 
@@ -19,9 +19,31 @@ client.on_message_create do |payload|
       client.create_message(payload.channel_id, "Restarting")
       Process.exec("sh restart.sh", shell: true)
     else
-      client.create_message(payload.channel_id, "Did you really think you'd be able to do this")
+      client.create_message(payload.channel_id, "https://youtu.be/Hwz7YN1AQmQ")
     end
   end
 end
+
+client.on_message_create do |payload|
+  if payload.content.starts_with? PREFIX + "die"
+    if payload.author.id == 228290433057292288_u64
+      client.create_message(payload.channel_id, "See ya :wave::skin-tone-1:")
+      exit
+    else
+      client.create_message(payload.channel_id, "https://youtu.be/Hwz7YN1AQmQ")
+    end
+  end
+end
+
+client.on_message_create do |payload|
+  if payload.content.starts_with? PREFIX + "help" || PREFIX + "commands" || PREFIX + "cmds"
+    client.create_message(payload.channel_id, "Oh, hey, this is the rewritten version of CahBot, and here are the commands thus far
+
+__Cah's Commands__
+`A^die`: Kills the bot
+`A^restart`: Kills the bot, pulls some code, and reboots
+
+__Other Commands__
+`A^ping`: Makes sure the bot is even alive")
 
 client.run
