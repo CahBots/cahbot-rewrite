3fbfda7ba9ac87388ee39ac6c755f3b4548e517f
2003cah <2003cah@gmail.com>
2017-06-20 16:02:31 -0400

    Cah is sucking at typing today

--- code.cr
+++ code.cr
@@ -6,6 +6,8 @@ client = Discord::Client.new(token: TOKEN, client_id: 291390171151335424_u64)
 
 PREFIX = "A^"
 
+START = Time.utc_now
+
 client.on_message_create do |payload|
   if payload.content.starts_with? PREFIX + "ping"
     m = client.create_message(payload.channel_id, "Pong!")
@@ -60,12 +62,28 @@ Example: *#{stuff.list.first.example}*
 end
 
 client.on_message_create do |payload|
-  if payload.content.starts_with? PREFIX + "help" || PREFIX + "commands" || PREFIX + "cmds"
+  if payload.content.starts_with? PREFIX + "console"
+    if payload.author.id == 228290433057292288_u64
+      time = Time.utc_now - payload.timestamp
+      client.create_message(payload.channel_id, "Executed the console command in about #{time.total_milliseconds.round(0)}ms
+
+```
+#{`payload.content[10..-1]`}
+```")
+    else
+      client.create_message(payload.channel_id, "https://youtu.be/Hwz7YN1AQmQ")
+    end
+  end
+end
+
+client.on_message_create do |payload|
+  if payload.content.starts_with? PREFIX + "help"
     client.create_message(payload.channel_id, "Oh, hey, this is the rewritten version of CahBot, and here are the commands thus far
 
 __Cah's Commands__
 `A^die`: Kills the bot
 `A^restart`: Kills the bot, pulls some code, and reboots
+`A^console`: Do things in the console
 
 __Other Commands__
 `A^ping`: Makes sure the bot is even alive
